7065f4ca2e344c386383ab6b556519e96ab14def
lukaszracki <luki.racki@gmail.com>
2024-02-27 15:54:24 +0100

    test

---
+++ lab2/main.py
@@ -0,0 +1,35 @@
+lista = [1,2,3,4.5,'jol',[11,12],3+2]
+
+print(lista)
+
+lista.append(10)
+
+print(lista)
+
+lista.insert(3,7)
+
+print(lista)
+
+lista.pop(2)
+
+print(lista)
+
+lista.remove('jol')
+
+print(lista)
+
+lista.reverse()
+
+print(lista)
+
+lista2 = [2,4,10,9,102,99,7,12]
+
+print(lista2)
+
+lista2.sort()
+
+print(lista2)
+
+del lista[3]
+
+print(lista)
